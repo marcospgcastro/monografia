fe496d3c946da51da23999ec3d2eb8c970fb8f57
marcos <marcospgcastro@if.uff.br>
2018-03-02 03:34:43 -0300

    Binário e Código Fonte do programa Carlo, que realiza simulações de Monte Carlo

--- lib/carlo.c
+++ lib/carlo.c
@@ -7,8 +7,8 @@
         PRORAMA:  CARLO -  REALIZA SIMULAÇÕES DE MONTE CARLO PARA CAMPO MÉDIO, APLICADAS
                            AO MODELO DE ESPALHAMENTO DE RUMORES D.K., NO ÂMBITO DA INTERAÇÃO
                            LIVRE ENTRE INDIVÍDUOS, CAMPO MÉDIO.
-        MÉTODO:  CARLO
-        DATA: 01/03/2018
+        MÉTODO:  TRIVIAL
+        DATA: 02/03/2018
 */
 
 #include <string.h>
@@ -17,15 +17,13 @@
 #include <math.h>
 #include <time.h>
 
-#define MAX32 16807
-#define MAX64 4294967295
+#define MAX32 16807	// MÁXIMO INTEIRO PARA SISTEMAS DE 32 BITS
+#define MAX64 4294967295// MÁXIMO INTEIRO PARA SISTEMAS DE 64 BITS
 
 typedef struct{         //DEFINE TIPO DE VARIAVEL "NUMBER" QUE DARÁ FORMA AS ESTRUTURAS DE NUMEROS RANDÔMICOS
-
   int long seed;        // DECLARAÇÃO DE INTEIRO LONGO
   float numb;           // DECLARAÇÃO DE PONTO FLUTUANTE
 } number;               // NOME DA ESTRUTURA
-
 
 number random(int long val){            // FUNÇÃO QUE RETORNA UMA VARIAVEL DO TIPO "NUMBER"
 
@@ -36,28 +34,93 @@ number random(int long val){            // FUNÇÃO QUE RETORNA UMA VARIAVEL DO 
   return rnd;                           // RETORNA VALOR "RND"
 }
 
+int main(int arg, char* line[]) {       // PROGRAMA PRINCIPAL
 
-int main(void) {                        // PROGRAMA PRINCIPAL
+ float lambda = atof(line[1]);		// VALOR ATRIBUIDO A VARIAVEL LAMBDA
+ float alfa   = atof(line[2]);		// VALOR ATRIBUIDO A VARIAVEL ALFA
+ float delta  = atof(line[3]);		// VALOR ATRIBUIDO A VARIAVEL DELTA
+
+ int m;					// AUXILIAR PARA CONTAGEM DE INDIVÍDUOS COM ESTATUS 'R'
+ int n;					// AUXILIAR PARA CONTAGEM DE INDIVÍDUOS COM ESTATUS 'S'
+ int u;					// AUXILIAR PARA CONTROLE DE VARIAVEIS ALEATÓRIAS
 
  int L = 10e5;				// DECLARAÇÃO DA DIMENSÃO DO VECTOR POPULAÇÃO
  char pop[L];				// DECLARAÇÃO DE VECTOR DE POPULAÇÃO
 
+ srand((time)NULL);			// SEMENTE PARA GERAR SEQUENCIA PSEUDO-ALEATÓRIA PELA FUNÇÃO 'RAND'
  number cont;                           // DEFINE UM "CONTADOR" AUXILIAR PARA CONTROLE DAS SEMENTES
  cont.seed = (time)NULL;                // DEFINIÇÃO DE SEMENTE ALEATÓRIA
 
- for(int i=1; i<=L; i++) {
+typedef struct {			// DEFINIÇÃO DE ESTRUTURA DE DADOS PARA GERENCIAMENTO DE VALORES DE DENSIDADE DE INDIVÍDUOS
+   float I;				// VALOR ATRIBUIDO AOS INDIVÍDUOS DE ESTADO 'I' NA ESTRUTURA DE DADOS
+   float S;				// VALOR ATRIBUIDO AOS INDIVÍDUOS DE ESTADO 'S' NA ESTRUTURA DE DADOS
+   float R;				// VALOR ATRIBUIDO AOS INDIVÍDUOS DE ESTADO 'R' NA ESTRUTURA DE DADOS
+ } valores;				// VARIAVEL DO TIPO 'VALORES'
 
-   pop[i] = 'I';			// VECTOR DE POPULAÇÃO RECEBENDO INDIVÍDUOS IGNORANTES
-   cont = random(cont.seed);            // ESTRUTURA DE AUTO REFERENCIAMENTO ORIUNDA DO PROGRAMA RANDO
-   if(cont.numb <= 0.01) pop[i] = 'S';	// VECTOR DE POPULAÇÃO RECEBENDO INDIVÍDUOS ESPALHADORES
+ valores var[500+1];			// DEFINIÇÃO DE 'VAR[]' COMO ESTRUTURA DE DADOS
+
+ for(int i=0; i<=L; i++) {		// LOOPING DE VARREDURA
+   var[i].I = 0.00;			// ATRIBUI VALOR NULO PARA VECTOR DE ESTADOS 'I'
+   var[i].S = 0.00;			// ATRIBUI VALOR NULO PARA VECTOR DE ESTADOS 'S'
+   var[i].R = 0.00;			// ATRIBUI VALOR NULO PARA VECTOR DE ESTADOS 'R'
  }
 
- int n = 0;
- for(int i=1; i<=L; i++) {
-   if(pop[i] == 'S') n++;
+ for(int simu=0; simu<=100; simu++) {   	// LOOPING QUE REALIZA MÉDIA SOBRE SIMULAÇÕES
+
+   for(int i=1; i<=L; i++) {			// LOOPING DE VARREDURA SOBRE O VECTOR DE POPULAÇÃO
+
+     pop[i] = 'I';				// VECTOR DE POPULAÇÃO RECEBENDO INDIVÍDUOS IGNORANTES
+     cont = random(cont.seed);            	// ESTRUTURA DE AUTO REFERENCIAMENTO ORIUNDA DO PROGRAMA RANDO
+     if(cont.numb <= 0.01) pop[i] = 'S';	// VECTOR DE POPULAÇÃO RECEBENDO INDIVÍDUOS ESPALHADORES
+   }
+
+   n = 0;					// CONTAGEM DE INDIVÍDUOS NO ESTADO 'S'
+   for(int i=1; i<=L; i++) {			// LOOPING DE VARREDURA
+     if(pop[i] == 'S') n++;			// ATUALIZA VALOR PARA CADA NOVO INDIVÍDUO
+   }
+
+   for(int i=0; i<=500; i++) {			// LOOPING SOBRE O TOTAL DE SIMULAÇÕES REALIZADAS SOBRE O SISTEMA
+
+     for(int j=0; j<=L; j++) {			// LOOPING DE VARREDURA SOBRE O VECTOR DE POPULAÇÃO
+
+       cont = random(cont.seed);		// GERADOR CONGRUENCIAL LINEAR, GCL, DE PSEUD-ALEATÓRIOS
+       do { u = rand() % L; } while(u == L);	// AUXILIAR PARA SORTEAR INDIVÍDUO SOBRE O QUAL ACONTECERÁ A INTERAÇÃO: PESSOA X PESSOA
+
+
+       if( pop[j] == 'I' && pop[u] == 'S' ) {	// CONDIÇÃO (1), [I]+[S]--->[S]+[S]: COM PROBABILIDADE LAMBDA
+         if( cont.numb <= lambda ) pop[j] = 'S';// TESTA A CONDIÇÃO PELO GCL
+       }
+       if( pop[j] == 'S' && pop[u] == 'S' ) {	// CONDIÇÃO (2), [S]+[S]--->[S]+[R]: COM PROBABILIDADE ALFA
+         if( cont.numb <= alfa ) pop[j] = 'R';	// TESTA A CONDIÇÃO PELO GCL
+       }
+       if( pop[j] == 'S' && pop[u] == 'R' ) {	// CONDIÇÃO (3), [S]+[R]--->[R]+[R]: COM PROBABILIDADE ALFA
+         if( cont.numb <= alfa ) pop[j] = 'R';	// TESTA A CONDIÇÃO PELO GCL
+       }
+       if( pop[j] == 'S' ) {			// CONDIÇÃO (4), [S]--->[R]: COM PROBABILIDADE DELTA
+         if( cont.numb <= delta ) pop[j] = 'R'; // TESTA A CONDIÇÃO PELO GCL
+       }
+     }
+
+     n = 0;					// CONTAGEM DE INDIVÍDUOS NO ESTADO 'S'
+     for(int i=0; i<=L; i++) {			// LOOPING DE VARREDURA
+       if(pop[i] == 'S') n++;			// ATUALIZA VALOR PARA CADA NOVO INDIVÍDUO
+     }
+     m = 0;					// CONTAGEM DE INDIVÍDUOS NO ESTADO 'R'
+     for(int i=0; i<=L; i++) {			// LOOPING DE VARREDURA
+       if(pop[i] == 'R') m++;			// ATUALIZA VALOR PARA CADA NOVO INDIVÍDUO
+     }
+
+     var[i].I = var[i].I + (1-(float)m/L-(float)n/L)/100;	// GUARDA VALOR DE DENSIDADE DE ÍNDIVÍDUOS NO ESTADO 'I' AO FINAL DO PERCORRIMENTO
+     var[i].S = var[i].S + (float)n/(L*100);                    // GUARDA VALOR DE DENSIDADE DE ÍNDIVÍDUOS NO ESTADO 'S' AO FINAL DO PERCORRIMENTO
+     var[i].R = var[i].R + (float)m/(L*100);                    // GUARDA VALOR DE DENSIDADE DE ÍNDIVÍDUOS NO ESTADO 'R' AO FINAL DO PERCORRIMENTO
+   }
  }
 
- printf("\n	Percentual de indivíduos espalhadores no início das simulações: %.10f %%\n\n ",100*(float)n/L);
+ for(int i=0; i<=500; i++) {			// RELATÓRIO DE SAIDA DE DADOS
+   printf("%.10f\t",var[i].I);			// DENSIDADE DE ESTADOS 'I'
+   printf("%.10f\t",var[i].S);			// DENSIDADE DE ESTADOS 'S'
+   printf("%.10f\n",var[i].R);			// DENSIDADE DE ESTADOS 'R'
+ }
 
  return 0;
 }
